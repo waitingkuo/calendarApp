9f3adf8deedc2d3a57c01f180e4063f881025767
alexsu1212 <alexpp44@gmail.com>
2014-07-23 10:47:14 +0000

    first commit

---
+++ demonstration/client/demonstration.js
@@ -0,0 +1,51 @@
+CalEvents = new Meteor.Collection('calevents');
+Session.setDefault("editing_caleventlevent", null);
+Session.setDefault("showEditEvent", false);
+Session.setDefault("lastMod", null);
+
+// Meteor.Router.add({
+//     "/": "homepage",
+//     "calendar": "calendar"
+// })
+
+Router.configure({
+    layoutTemplate: 'layout'
+});
+
+Router.map( function () {
+  this.route('homepage', {path: '/'});
+  this.route('calendar', {path: '/calendar'});
+});
+
+Template.calendar.lastMod = function() {
+    return Session.get('lastMod');
+}
+
+Template.calendar.rendered = function() {
+    $('#calendar').fullCalendar({
+        dayClick: function(date, jsEvent, view) {
+            CalEvents.insert({title: 'New Event', start: date, end: date});
+            Session.set('lastMod', new Date());
+        },
+        eventClick: function(calEvent, jsEvent, view) {
+
+        },
+        events: function(start, end, callback) {
+            var events = [];
+            calEvents = CalEvents.find();
+            calEvents.forEach(function(evt) {
+                console.log(evt);
+                events.push({
+                    id: evt._id,
+                    title: evt.title,
+                    start: evt.start,
+                    end: evt.end
+                });
+            })
+            callback(events);
+        }
+    });
+}
+
+
+
